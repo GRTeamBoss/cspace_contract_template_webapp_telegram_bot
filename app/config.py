e3d06996d2592ee9cd828c69a37f852bef1c8d16
GRTeamBoss <rabbitwow97@gmail.com>
2025-08-20 22:58:29 +0500

    adding env

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     POSTGRE_HOST = os.getenv('POSTGRE_HOST', 'localhost')
     POSTGRE_PORT = os.getenv('POSTGRE_PORT', '5432')
     SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRE_USER}:{POSTGRE_PASSWORD}@{POSTGRE_HOST}:{POSTGRE_PORT}/{POSTGRE_DBNAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', 'admin')
     ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'password')
     SECRET_KEY = 'your_secret_key'
